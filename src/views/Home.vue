4d60350400700bfefd4c2f1bd29f8143c31cff7b
joeyang1512 <joeyang1512@gmail.com>
2020-11-16 22:05:28 +0800

    初步搭建脚手架

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,14 +8,17 @@
 <script>
 // @ is an alias to /src
 // 测试api的使用
-import { getOtherInfo } from '@/ajax/index'
+// import { getOtherInfo } from '@/ajax/index'
+// 取消末尾换行eslint
+// import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
 
   },
+
   created () {
-    getOtherInfo().then(res => {
+    this.$http.getOtherInfo().then(res => {
       console.log(res)
     })
   }
