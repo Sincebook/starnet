9cd3b97eddd7c98857d099bd2c194f06074df56c
joeyang1512 <joeyang1512@gmail.com>
2020-11-30 18:09:13 +0800

    对接人才-公司-工作展示页面

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
         changeOrigin: true, // 虚拟的站点需要更管origin
         pathRewrite: { // 重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
           '^/api': ''
-        },
+        }
       },
       '/bingpic': {
         target: 'https://qnets.cn/bingpic/', // API服务器的地址
