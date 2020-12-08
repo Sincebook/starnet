e3d80d505da1bb4714b4d00c55b3644f8ced5e91
joeyang1512 <joeyang1512@gmail.com>
2020-12-08 09:20:16 +0800

    合并公司详情

--- src/ajax/index.js
+++ src/ajax/index.js
@@ -15,6 +15,12 @@ export {
   findByUptime,
   findByCategory,
   findByTwo,
+  getComInfoById,
+  getComVideoByUserId,
+  getComPhotosByUserId,
+  getComGradeByUserId,
+  showFunsNumsByUserId,
+  sendMessageToId,
   fingComInfo,
   findComByName
 }
