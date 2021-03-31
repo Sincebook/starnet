3967803443e53fde7d8c17fd62c8c295c0408106
LAPTOP-GPEDA3B7\ZLY <zly071065@163.com>
2021-03-31 13:21:38 +0800

    企业信息修改

--- src/components/corporateCenter/info.vue
+++ src/components/corporateCenter/info.vue
@@ -18,11 +18,48 @@
       </div>
     </div>
     <div class="info" v-if="companyInfo.status === 2">
-      <el-form ref="ruleForm" label-width="100px" class="demo-ruleForm">
-        <el-form-item label="公司Logo：" class="logo">
-          <el-image class="companyLogo" fit="cover" :src="companyInfo.logo">
-          </el-image
-        ></el-form-item>
+      <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px" class="demo-ruleForm">
+        <!--公司logo-->
+        <el-form-item label="公司Logo：" class="logo" prop="logo">
+          <!-- <el-image class="companyLogo" fit="cover" :src="companyInfo.logo">
+          </el-image> -->
+          <el-upload
+            class="avatar-uploader"
+            :http-request="upload"
+            action=""
+            :show-file-list="false"
+            :on-change='changeUpload'
+          >
+          <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i> </el-upload>
+          <el-dialog title="图片剪裁" :visible.sync="dialogVisible" append-to-body>
+            <div class="cropper-content">
+              <div class="cropper" style="text-align:center">
+                <vueCropper
+                  ref="cropper"
+                  :img="option.img"
+                  :outputSize="option.size"
+                  :outputType="option.outputType"
+                  :info="true"
+                  :full="option.full"
+                  :canMove="option.canMove"
+                  :canMoveBox="option.canMoveBox"
+                  :original="option.original"
+                  :autoCrop="option.autoCrop"
+                  :fixed="option.fixed"
+                  :fixedNumber="option.fixedNumber"
+                  :centerBox="option.centerBox"
+                  :infoTrue="option.infoTrue"
+                  :fixedBox="option.fixedBox"
+                ></vueCropper>
+              </div>
+            </div>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogVisible = false">取 消</el-button>
+              <el-button type="primary" @click="finish" :loading="loading">确认</el-button>
+            </div>
+          </el-dialog>
+        </el-form-item>
         <el-form-item label="组织机构码：">
           <span>{{ companyInfo.organizationCode }}</span>
         </el-form-item>
@@ -47,34 +84,257 @@
         <el-form-item label="经营范围：">
           <span>{{ companyInfo.managementRange }}</span>
         </el-form-item>
-        <el-form-item label="代表作：">
-          <span>{{ companyInfo.opus }}</span>
+        <!--代表作-->
+        <el-form-item label="代表作：" prop="opus">
+          <el-input
+              v-model="ruleForm.opus"
+              placeholder="请输入公司代表作"
+              type="textarea"
+              :rows="2"
+            ></el-input>
+          <!-- <span>{{ companyInfo.opus }}</span> -->
         </el-form-item>
-        <el-form-item label="公司简介：">
-          <div class="textarea">{{ companyInfo.description }}</div>
+        <!--公司简历-->
+        <el-form-item label="公司简介：" prop="description">
+            <el-input
+              v-model="ruleForm.description"
+              placeholder="请输入公司简介"
+              type="textarea"
+              :rows="7"
+            ></el-input>
+          <!-- <div class="textarea">{{ companyInfo.description }}</div> -->
         </el-form-item>
-        <el-form-item label="公司照片：">
-          <el-image class="companyIdcard" fit="cover" :src="companyInfo.image">
-          </el-image
-        ></el-form-item>
+        <!--公司照片-->
+        <el-form-item label="公司照片：" prop="image">
+          <el-upload
+            class="avatar-uploader"
+            :http-request="upload1"
+            action=""
+            :show-file-list="false"
+            :on-change='changeUpload1'
+          >
+          <img v-if="imageUrl1" :src="imageUrl1" class="companyIdcard" />
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i> </el-upload>
+          <el-dialog title="图片剪裁" :visible.sync="dialogVisible1" append-to-body>
+            <div class="cropper-content">
+              <div class="cropper" style="text-align:center">
+                <vueCropper
+                  ref="cropper"
+                  :img="option.img1"
+                  :outputSize="option.size"
+                  :outputType="option.outputType"
+                  :info="true"
+                  :full="option.full"
+                  :canMove="option.canMove"
+                  :canMoveBox="option.canMoveBox"
+                  :original="option.original"
+                  :autoCrop="option.autoCrop"
+                  :fixed="option.fixed"
+                  :fixedNumber="option.fixedNumber1"
+                  :centerBox="option.centerBox"
+                  :infoTrue="option.infoTrue"
+                  :fixedBox="option.fixedBox"
+                ></vueCropper>
+              </div>
+            </div>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogVisible1 = false">取 消</el-button>
+              <el-button type="primary" @click="finish1" :loading="loading1">确认</el-button>
+            </div>
+          </el-dialog>
+          <!-- <el-image class="companyIdcard" fit="cover" :src="companyInfo.image">
+          </el-image -->
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" class="submit" :disabled="flag2" @click="submitForm()"
+              >保存</el-button
+            >
+        </el-form-item>
       </el-form>
     </div>
   </div>
 </template>
 
 <script>
+import { companyInfo, modifyCompanyinfo } from '../../ajax/index';
+import { VueCropper } from 'vue-cropper';
 import { mapState } from 'vuex';
 export default {
+  components: {
+    VueCropper
+  },
   props: ['companyInfo'],
+  data() {
+    return {
+      option: {
+        img: '', // 裁剪图片的地址
+        img1: '',
+        info: true, // 裁剪框的大小信息
+        outputSize: 0.8, // 裁剪生成图片的质量
+        outputType: 'jpeg', // 裁剪生成图片的格式
+        canScale: true, // 图片是否允许滚轮缩放
+        autoCrop: true, // 是否默认生成截图框
+        // autoCropWidth: 300, // 默认生成截图框宽度
+        // autoCropHeight: 200, // 默认生成截图框高度
+        fixedBox: false, // 固定截图框大小 不允许改变
+        fixed: true, // 是否开启截图框宽高固定比例
+        fixedNumber: [1, 1], // 截图框的宽高比例
+        fixedNumber1: [270, 120], // 截图框的宽高比例
+        full: true, // 是否输出原图比例的截图
+        canMoveBox: true, // 截图框能否拖动
+        original: false, // 上传图片按照原始比例渲染
+        centerBox: false, // 截图框是否被限制在图片里面
+        infoTrue: true // true 为展示真实输出图片宽高 false 展示看到的截图框宽高
+      },
+      dialogVisible: false,
+      dialogVisible1: false,
+      fileinfor: '',
+      imageUrl: '',
+      loading: false,
+      flag: false,
+      fileinfor1: '',
+      imageUrl1: '',
+      loading1: false,
+      flag1: false,
+      status: 1,
+      flag2: false,
+    ruleForm: {
+      logo: '', // 公司logo
+      opus: '', // 公司代表作
+      description: '', // 公司描述
+      image: '' // 公司照片
+    },
+    rules: {
+      logo: [
+        { required: true, message: '公司Logo不能为空', trigger: 'change' }
+      ],
+      description: [
+          { required: true, message: '公司简介不能为空', trigger: 'blur' }
+      ],
+      image: [
+          { required: true, message: '公司图片不能为空', trigger: 'change' }
+      ]
+    }
+  };
+},
   methods: {
+    async upload(content) {
+      this.ruleForm.logo = content.file;
+    },
+    async upload1(content) {
+      this.ruleForm.image = content.file;
+    },
     goCelebrity() {
       this.$emit('goCelebrity');
+    },
+    changeUpload(file, fileList) {
+      const isJPG = file.raw.type === 'image/jpeg' || 'image/png';
+      const isLt2M = file.size / 1024 / 1024 < 20;
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG/png 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 20MB!');
+        return false;
+      }
+      this.fileinfor = file;
+      // 上传成功后将图片地址赋值给裁剪框显示图片
+      this.$nextTick(() => {
+        this.option.img = URL.createObjectURL(file.raw);
+        this.dialogVisible = true;
+      });
+      },
+    finish() {
+        this.$refs.cropper.getCropBlob((data) => {
+          let file = new window.File([data], this.fileinfor.name, { type: 'image/jpeg' });
+          this.imageUrl = window.URL.createObjectURL(data);
+          this.ruleForm.logo = file;
+          console.log(this.ruleForm.logo);
+        });
+        this.dialogVisible = false;
+      },
+    changeUpload1(file, fileList) {
+      const isJPG = file.raw.type === 'image/jpeg' || 'image/png';
+      const isLt2M = file.size / 1024 / 1024 < 20;
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG/png 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 20MB!');
+        return false;
+      }
+      this.fileinfor1 = file;
+      // 上传成功后将图片地址赋值给裁剪框显示图片
+      this.$nextTick(() => {
+        this.option.img1 = URL.createObjectURL(file.raw);
+        this.dialogVisible1 = true;
+      });
+      },
+      finish1() {
+        this.$refs.cropper.getCropBlob((data) => {
+          let file = new window.File([data], this.fileinfor1.name, { type: 'image/jpeg' });
+          this.imageUrl1 = window.URL.createObjectURL(data);
+          this.ruleForm.image = file;
+          console.log(this.ruleForm.image);
+        });
+        this.dialogVisible1 = false;
+      },
+    submitForm() {
+      this.$refs.ruleForm.validate((valid) => {
+        if (valid) {
+          this.flag2 = true;
+          console.log(this.ruleForm.logo);
+          console.log(this.ruleForm.image);
+          console.log(this.ruleForm.opus);
+          console.log(this.ruleForm.description);
+          modifyCompanyinfo(this.ruleForm).then(res => {
+            if (res.code === '0') {
+              this.$message({
+                message: '修改成功',
+                type: 'success'
+              });
+            } else {
+              this.$message.error(res.errMsg);
+            }
+            this.flag2 = false;
+          }).catch(err => {
+            this.flag2 = false;
+            this.$message.error(err);
+            return err;
+          });
+        }
+      });
     }
   },
   computed: {
     ...mapState({
       userinfo: (state) => state.userinfo
     })
+  },
+  watch: {
+    userinfo: function (newVal, oldVal) {
+      this.status = newVal.status;
+      console.log(this.status);
+    },
+    deep: true
+  },
+  created() {
+    this.status = this.userinfo.user.status;
+    console.log(this.userinfo.user.id);
+    if (this.userinfo.user.status !== 1) {
+      companyInfo({
+        id: this.userinfo.user.id
+      }).then(res => {
+        if (res.code === '0') {
+        this.imageUrl = (res.data.logo === null ? '' : res.data.logo);
+        this.ruleForm.opus = (res.data.opus === null ? '' : res.data.opus);
+        this.ruleForm.description = (res.data.description === null ? '' : res.data.description);
+        this.imageUrl1 = (res.data.image === null ? '' : res.data.image);
+        this.ruleForm.image = res.data.image;
+        this.ruleForm.logo = res.data.logo;
+        }
+      });
+    }
   }
 };
 </script>
@@ -159,5 +419,46 @@ export default {
     align-items: center;
     height: 60px;
   }
+  /deep/.avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+/deep/ .avatar-uploader {
+    height: 54px;
 }
+.avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 108px;
+    height: 108px;
+    line-height: 108px;
+    text-align: center;
+}
+.avatar {
+    width:  54px;
+    height: 54px;
+    border-radius: 5px;
+    display: block;
+    cursor: pointer;
+}
+.companyIdcard {
+    height: 108px;
+    width: 156px;
+    border-radius: 5px;
+    display: block;
+    cursor: pointer;
+}
+}
+  .cropper-content {
+    .cropper {
+        width: auto;
+        height: 300px;
+    }
+  }
+  .submit {
+    margin-top: 50px;
+  }
 </style>
